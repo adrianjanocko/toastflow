39ee9eafb09f56d32060b8325e9117f0db13fb0e
Adrián Janočko <adonikeoffice@gmail.com>
2025-12-08 17:49:26 +0100

    -- vue-toastflow v1.0.5
    .gitignore, toastflow.es.js, toastflow.umd.js, vue-toastflow.css
    * removed dist and always building it automatically before publishing
    
    package.json
    * adjusted prepack command & added files
    
    main.ts
    * prettier formatting
    
    Took 14 minutes

--- packages/playground/vue/src/main.ts
+++ packages/playground/vue/src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import './globals.css'
+import './globals.css';
 
-import App from './App.vue'
-import { createToastflow } from 'vue-toastflow'
+import App from './App.vue';
+import { createToastflow } from 'vue-toastflow';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(createToastflow())
+app.use(createPinia());
+app.use(createToastflow());
 
-app.mount('#app')
+app.mount('#app');
